c83d465d6a41e90a38626a5a6a58479f1660c796
Terry Qu <60083573+terryquu@users.noreply.github.com>
2025-09-22 12:14:57 -0700

    added socrata

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("hello")
+import pandas as pd
+import numpy as np
+from sodapy import Socrata
+
+client = Socrata("data.ny.gov", None)
+
+results = client.get("khy8p-hchm", limit=2000)
+
+results_df = pd.DataFrame.from_records(results)
+
