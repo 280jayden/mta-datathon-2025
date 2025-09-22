ac6a092a2f45f467158613051f018580bd68f03e
Terry Qu <60083573+terryquu@users.noreply.github.com>
2025-09-22 20:29:32 +0000

    updated access_data and main to read the data

--- access_data.py
+++ access_data.py
@@ -1,12 +1,22 @@
 import pandas as pd
-import requests
+import requests as rq
+import os
 
-def main():
+def fetch_data():
     url = "https://data.ny.gov/api/odata/v4/ki2b-sg5y"
-    response = requests.get(url)
-    data = response.json()
-    df = pd.DataFrame(data['value'])
-    print((df.head()))
+    all_data = []
+
+    while url:
+        response = rq.get(url)
+        response.raise_for_status()
+        data = response.json()
+        all_data.extend(data['value'])
+        url = data.get('@odata.nextLink')
+    
+    df = pd.DataFrame(all_data)
+
+    os.makedirs("data", exist_ok=True)
+    df.to_csv("data/data.csv",index=False)
 
 if __name__ == "__main__":
-    main()
+    fetch_data()
--- main.py
+++ main.py
@@ -2,7 +2,10 @@ import pandas as pd
 import numpy as np
 
 def main():
-    df = pd.DataFrame(data)
+    df = pd.read_csv("data/data.csv")
+
+    print(df.head())
+    print(df.describe())
 
 if __name__ == "__main__":
     main()
